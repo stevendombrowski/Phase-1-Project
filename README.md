6450a21c5fe1c05f7f2f5ca959489e7d44fd0089
Steven <dombrowskis153@yahoo.com>
2022-04-05 01:32:56 -0400

    final pushes

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ The main purpose for this website is to act as a secondary Craiglists but for ad
 It has on the main tab a list of popular dogs that are on the site(just place holder for creation of the site)
 The second tab is an integrated google maps(to show/search for nearby adoption centers)
 The last is a placeholder about section(to describe the organization that would be behind the actual site)
+
+
+
+The buttons on the page were designed by me.
+
+All pictures of dogs were captured from https://www.pexels.com/search/dogs/
+
+The Banner was captured from https://www.wisdompanel.com/en-gb/the-process?awc=16376_1644930542_1335861e1b5c3f978d42328fa03ad50f
+
+The page icon is from https://www.123rf.com/photo_69001806_cat-and-dog-icon-black-icon-on-transparent-background.html
--- java.js
+++ java.js
@@ -1,4 +1,5 @@
 
+
 function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -31,3 +32,42 @@ function dark(){
   element.classList.toggle("darkmode");
 }
 
+const fetchDogBreeds = async () => {
+
+  const response = await fetch('https://api.thedogapi.com/v1/breeds');
+  const dogBreeds = await response.json();
+  populateDogSelect(dogBreeds);
+
+}
+
+const populateDogSelect = (breeds) => {
+  const select = document.querySelector('.breed-select');
+  const breedOptions = breeds.map(breed => {
+    const option = document.createElement('option');
+    option.text = breed.name;
+    option.value = breed.id;
+    return option;
+
+  })
+  breedOptions.forEach(breedOption => {
+    select.appendChild(breedOption);
+  })
+}
+
+const fillImage = (imageUrl) => {
+  document.querySelector('#dogimage').setAttribute('src', imageUrl);
+}
+
+const getDogByBreed = async (breedId) => {
+  const [data] = await fetch('https://api.thedogapi.com/v1/images/search?include_breed=1&breed_id=' + breedId).then((data) => data.json())
+  const {url: imageUrl, breeds} = data;
+  fillImage(imageUrl);
+
+}
+
+const changeDog = () => {
+
+  console.log(event.target.value)
+  getDogByBreed(event.target.value);
+}
+fetchDogBreeds()
