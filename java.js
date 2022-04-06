92fc1c8466954efc33ae75b5a9da077da04bac6a
stevendombrowski <88370598+stevendombrowski@users.noreply.github.com>
2022-04-06 02:01:22 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
 
 
 function openTab(evt, tabName) {
-  // Declare all variables
+ 
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
+  
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -16,7 +16,7 @@ function openTab(evt, tabName) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
+ 
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
